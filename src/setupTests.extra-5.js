f6adce6485778f1ab19786fad59ee973f8bf8986
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-28 12:44:35 -0600

    exercises/13-integration-testing

---
+++ src/setupTests.extra-5.js
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom/extend-expect'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
+import {server} from 'test/server'
+import * as usersDB from 'test/data/users'
+import * as listItemsDB from 'test/data/list-items'
+import * as booksDB from 'test/data/books'
+
+// we don't need the profiler in tests
+jest.mock('components/profiler')
+
+// enable API mocking in test runs using the same request handlers
+// as for the client-side mocking.
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
+
+// real times is a good default to start, individual tests can
+// enable fake timers if they need.
+beforeEach(() => jest.useRealTimers())
+
+// general cleanup
+afterEach(async () => {
+  queryCache.clear()
+  await Promise.all([
+    auth.logout(),
+    usersDB.reset(),
+    booksDB.reset(),
+    listItemsDB.reset(),
+  ])
+})
